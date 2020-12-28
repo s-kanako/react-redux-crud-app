9814f6c72040e0a793c5c7b7fb6b9726d6775b8b
KANAKO <s.ka10na26ko@gmail.com>
2020-12-29 00:41:10 +0900

    create class component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <h1>HELOOOO</h1>
-      </React.Fragment>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <h1>HELOOOO</h1>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const App = () => {
+  return (
+      <div>
+        <Cat />
+        <Cat />
+        <Cat />
+        <Cat />
+
+      </div>
+  )
+}
+const Cat = () => {
+  return <div>Meow!!</div>
 }
 
 export default App;
