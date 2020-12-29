6ddcaea37ff89aa07db1ed51db1afa2a8a3a99ca
KANAKO <s.ka10na26ko@gmail.com>
2020-12-29 15:39:08 +0900

    learn component props

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <h1>HELOOOO</h1>
-//       </React.Fragment>
-//     );
-//   }
-// }
+import React from 'react';
 
 const App = () => {
+  const profiles = [
+    {
+      name: "Taro", age: 10
+    },
+    {
+      name: "Hanako",age: 5
+    },
+    {
+      name: "Mery"
+    }
+  ]
   return (
       <div>
-        <Cat />
-        <Cat />
-        <Cat />
-        <Cat />
-
+        {
+          profiles.map((profile,index) => {
+            return <User name={profile.name} age ={profile.age} key={index} />
+          })
+        }
       </div>
   )
 }
-const Cat = () => {
-  return <div>Meow!!</div>
+const User = (props) => {
+  return <div>Hi I am {props.name}, and {props.age} years old !!</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
